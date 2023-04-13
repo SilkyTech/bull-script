df64cc28e010b2178156eaec8fc5a388f8cae303
Silkoway <ethancai920@gmail.com>
2023-04-13 20:37:35 +0800

    added better error reporting

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,8 +1,10 @@
 use std::collections::HashMap;
 
 use crate::{
+    ctwl,
+    error::error_at,
     lexer::Lexer,
-    parser::{BinaryOperator, Expr, LiteralType, Parser, UnaryOperator},
+    parser::{BinaryOperator, Expr, ExprWL, LiteralType, Parser, UnaryOperator},
 };
 
 fn unbox<T>(value: Box<T>) -> T {
@@ -17,20 +19,31 @@ macro_rules! insert_variables_from_run_proc {
         }
     }};
 }
+macro_rules! qerror {
+    ($p: expr, $f: expr) => {
+        error_at(
+            &$p.filen.clone(),
+            &$p.linen.clone(),
+            &$p.charn.clone(),
+            &$f.to_string(),
+        )
+    };
+}
 
 pub struct Interpreter {}
 impl Interpreter {
     pub fn run_proc_name(
         &mut self,
-        expr: Expr,
-        mut variables: HashMap<Vec<String>, Expr>,
+        exprwl: ExprWL,
+        mut variables: HashMap<Vec<String>, ExprWL>,
         namespace: Vec<String>,
-    ) -> (Expr, HashMap<Vec<String>, Expr>) {
+    ) -> (ExprWL, HashMap<Vec<String>, ExprWL>) {
+        let expr = exprwl.clone().expr.clone();
         if let Expr::Call(name, args) = expr {
             if name.starts_with(vec!["builtin".to_string()].as_slice()) {
                 let subcmd = name.iter().nth(1);
                 match subcmd {
-                    None => panic!("No subcommand for 'builtin' namespace"),
+                    None => qerror!(exprwl, &"No subcommand for 'builtin' namespace"),
                     Some(v) => match v.as_str() {
                         "printval" => {
                             assert_eq!(args.len(), 1, "Argument length required to be one: usage `builtin.printstr($val)`");
@@ -65,17 +78,20 @@ impl Interpreter {
                                 )
                             );
                         }
-                        _ => panic!("No subcommand called '{}' in builtin functions", v),
+                        _ => qerror!(
+                            exprwl,
+                            format!("No subcommand called '{}' in builtin functions", v)
+                        ),
                     },
                 }
                 return (
-                    Expr::Literal(LiteralType::Null, "".to_string()),
+                    ctwl!(Expr::Literal(LiteralType::Null, "".to_string()), exprwl),
                     variables.clone(),
                 );
             } else {
                 match variables.get(&name) {
                     Some(v) => {
-                        if let Expr::Proc(_name, _arg, _prog) = v {
+                        if let Expr::Proc(_name, _arg, _prog) = &v.expr {
                             let vars = self.run_proc(
                                 args.clone(),
                                 v.clone(),
@@ -88,10 +104,10 @@ impl Interpreter {
 
                             return (vars.0.clone(), variables.clone());
                         } else {
-                            panic!("{} is not a proc", name.join("."))
+                            qerror!(exprwl, format!("{} is not a proc", name.join(".")))
                         }
                     }
-                    None => panic!("{} is not defined", name.join(".")),
+                    None => qerror!(exprwl, format!("{} is not defined", name.join("."))),
                 }
             }
         } else {
@@ -101,11 +117,12 @@ impl Interpreter {
 
     pub fn run_code(
         &mut self,
-        code: Vec<Expr>,
-        mut variables: HashMap<Vec<String>, Expr>,
+        code: Vec<ExprWL>,
+        mut variables: HashMap<Vec<String>, ExprWL>,
         namespace: Vec<String>,
-    ) -> (Expr, HashMap<Vec<String>, Expr>) {
-        for expr in code {
+    ) -> (ExprWL, HashMap<Vec<String>, ExprWL>) {
+        for exprwl in code {
+            let expr = exprwl.clone().expr;
             if let Expr::Import(rel, path) = expr.clone() {
                 if rel {
                     // TODO: do relative imports
@@ -113,7 +130,7 @@ impl Interpreter {
                     let raw = match path.as_str() {
                         "std" => include_str!("lib/std.bs"),
                         "math" => include_str!("lib/math.bs"),
-                        _ => panic!("TODO: add better error reporting | invalid std import"),
+                        _ => qerror!(exprwl, format!("Invaild std import!")),
                     }
                     .to_string()
                     .replace("\r", &"");
@@ -128,7 +145,7 @@ impl Interpreter {
                     let vars = inter.run_program(program, false, namespace.clone());
                     for (k, v) in vars {
                         if variables.contains_key(&k) {
-                            panic!("TODO: add better error reporting | variable already defined")
+                            qerror!(exprwl, format!("Variable already defined"));
                         }
                         let new_namespace = namespace.clone();
                         let new_namespace = new_namespace.iter().chain(&k).map(|f| f.clone());
@@ -138,12 +155,12 @@ impl Interpreter {
                 }
             } else if let Expr::Proc(name, _args, _prog) = expr.clone() {
                 if variables.contains_key(&name) {
-                    panic!("TODO: add better error reporting | variable already defined")
+                    qerror!(exprwl, format!("Variable already defined"));
                 } else {
                     let new_namespace = namespace.clone();
                     let new_namespace = new_namespace.iter().chain(&name).map(|f| f.clone());
                     let new_namespace: Vec<String> = new_namespace.collect();
-                    variables.insert(new_namespace, expr);
+                    variables.insert(new_namespace, exprwl);
                 }
             } else if let Expr::Namespace(name, space) = expr.clone() {
                 let new_namespace = namespace.clone();
@@ -156,14 +173,17 @@ impl Interpreter {
                     }
                 }
             } else if let Expr::Call(_name, _args) = expr.clone() {
-                self.run_proc_name(expr, variables.clone(), namespace.clone());
+                self.run_proc_name(exprwl, variables.clone(), namespace.clone());
             } else if let Expr::VariableDeclaration(name, expr) = expr.clone() {
                 if !variables.contains_key(&name) {
                     variables.insert(name, unbox(expr));
                 } else {
-                    panic!(
-                        "Variable named \"{}\" has already been defined",
-                        name.join(".")
+                    qerror!(
+                        exprwl,
+                        format!(
+                            "Variable named \"{}\" has already been defined",
+                            name.join(".")
+                        )
                     );
                 }
             } else if let Expr::ConstantDeclaration(name, expr) = expr.clone() {
@@ -173,9 +193,12 @@ impl Interpreter {
                     let new_namespace: Vec<String> = new_namespace.collect();
                     variables.insert(new_namespace, unbox(expr));
                 } else {
-                    panic!(
-                        "Variable named \"{}\" has already been defined",
-                        name.join(".")
+                    qerror!(
+                        exprwl,
+                        format!(
+                            "Variable named \"{}\" has already been defined",
+                            name.join(".")
+                        )
                     );
                 }
             } else if let Expr::VariableSet(name, expr) = expr.clone() {
@@ -183,10 +206,13 @@ impl Interpreter {
                 if variables.contains_key(&name) {
                     let newval =
                         self.resolve_variable(unbox(expr), variables.clone(), namespace.clone());
-                    let newval = Expr::Literal(newval.0, newval.1);
+                    let newval = ctwl!(Expr::Literal(newval.0, newval.1), exprwl);
                     variables.insert(name, newval);
                 } else {
-                    panic!("Variable named \"{}\" isn't defined", name.join("."));
+                    qerror!(
+                        exprwl,
+                        format!("Variable named \"{}\" isn't defined", name.join("."))
+                    );
                 }
             } else if let Expr::For(name, start, end, prog) = expr.clone() {
                 let start =
@@ -200,7 +226,10 @@ impl Interpreter {
                             let mut new_vars = variables.clone();
                             new_vars.insert(
                                 name.clone(),
-                                Expr::Literal(LiteralType::Number, i.to_string()),
+                                ctwl!(
+                                    Expr::Literal(LiteralType::Number, i.to_string()),
+                                    exprwl.clone()
+                                ),
                             );
                             let res =
                                 self.run_code(prog.clone(), new_vars.clone(), namespace.clone());
@@ -218,15 +247,20 @@ impl Interpreter {
         }
 
         return (
-            Expr::Literal(LiteralType::Null, "".to_string()),
+            ExprWL {
+                expr: (Expr::Literal(LiteralType::Null, "".to_string())),
+                linen: 0,
+                charn: 0,
+                filen: "".to_string(),
+            },
             variables.clone(),
         );
     }
 
     pub fn resolve_math(
         &mut self,
-        val: Expr,
-        variables: HashMap<Vec<String>, Expr>,
+        val: ExprWL,
+        variables: HashMap<Vec<String>, ExprWL>,
         namespace: Vec<String>,
     ) -> (LiteralType, String) {
         fn to_num(v: (LiteralType, String)) -> f64 {
@@ -235,10 +269,10 @@ impl Interpreter {
             } else if let LiteralType::Boolean = v.0 {
                 return v.1.parse::<f64>().unwrap();
             } else {
-                panic!("")
+                panic!("resolve_math didn't get number or boolean")
             }
         }
-        if let Expr::Binary(op, left, right) = val {
+        if let Expr::Binary(op, left, right) = val.expr {
             match op {
                 BinaryOperator::Add => {
                     return (
@@ -400,7 +434,7 @@ impl Interpreter {
                     )
                 }
             }
-        } else if let Expr::Unary(op, right) = val {
+        } else if let Expr::Unary(op, right) = val.expr {
             match op {
                 UnaryOperator::Negative => {
                     return (
@@ -433,11 +467,11 @@ impl Interpreter {
 
     pub fn resolve_variable(
         &mut self,
-        y: Expr,
-        variables: HashMap<Vec<String>, Expr>,
+        y: ExprWL,
+        variables: HashMap<Vec<String>, ExprWL>,
         namespace: Vec<String>,
     ) -> (LiteralType, String) {
-        match y.clone() {
+        match y.clone().expr {
             Expr::Literal(_ty, v) => return (_ty, v),
             Expr::Unary(..) | Expr::Binary(..) => {
                 return self.resolve_math(y, variables.clone(), namespace.clone())
@@ -445,7 +479,10 @@ impl Interpreter {
             Expr::Identifier(name) => {
                 let var = variables.get(&name.clone());
                 match var {
-                    None => panic!("Variable \"{}\" doesn't exist", name.join(".")),
+                    None => qerror!(
+                        y,
+                        format!("Variable named \"{}\" hasn't been defined", name.join("."))
+                    ),
                     Some(v) => {
                         self.resolve_variable(v.clone(), variables.clone(), namespace.clone())
                     }
@@ -466,12 +503,12 @@ impl Interpreter {
 
     pub fn run_proc(
         &mut self,
-        args: Vec<Expr>,
-        func: Expr,
-        mut scope: HashMap<Vec<String>, Expr>,
+        args: Vec<ExprWL>,
+        func: ExprWL,
+        mut scope: HashMap<Vec<String>, ExprWL>,
         namespace: Vec<String>,
-    ) -> (Expr, HashMap<Vec<String>, Expr>) {
-        if let Expr::Proc(_name, pargs, prog) = func {
+    ) -> (ExprWL, HashMap<Vec<String>, ExprWL>) {
+        if let Expr::Proc(_name, pargs, prog) = func.expr {
             for (i, arg) in pargs.iter().enumerate() {
                 scope.insert(vec![arg.to_string()], args[i].clone());
             }
@@ -486,11 +523,11 @@ impl Interpreter {
 
     pub fn run_program(
         &mut self,
-        code: Expr,
+        code: ExprWL,
         main: bool,
         namespace: Vec<String>,
-    ) -> HashMap<Vec<String>, Expr> {
-        if let Expr::Program(p) = code {
+    ) -> HashMap<Vec<String>, ExprWL> {
+        if let Expr::Program(p) = code.expr {
             let ret = self.run_code(p, HashMap::new(), namespace.clone());
             if main {
                 let main = ret
